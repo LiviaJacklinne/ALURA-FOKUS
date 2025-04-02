8e2d186ab8e25e3ea6bddc86937bdb844e0ef92c
LiviaJacklinne <livia.jacklinne@gec.inatel.br>
2025-04-01 22:11:56 -0300

    feat: adicionando mensagem de texto + efeito dos botões + otimizando código

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const botaoIniciar = document.querySelector(".app__card-primary-button");
+const botoes = document.querySelectorAll(".app__card-button");
 
 const displayTempo = document.querySelector("#timer");
 const imagem = document.querySelector(".app__image");
@@ -14,18 +15,56 @@ const duracaoDescansoCurto = 300;
 const duracaoDescansoLong = 900;
 
 
-// Quando clicar no botão "focoBt", a variavel "data-contexto" deve ser setada com o CSS que está em "foco"
+// ouvindo os eventos do arquivo HTML quando clicado em um dos botões declarados
 focoBt.addEventListener("click", () => {
-    html.setAttribute("data-contexto", "foco");
-    imagem.setAttribute("src", "/imagens/foco.png");
+    alterarContexto("foco");
+    // ativando o CSS com foco no modo selecionado (classe estilizada no CSS - active)
+    focoBt.classList.add("active");
 });
 
 curtoBt.addEventListener("click", () => {
-    html.setAttribute("data-contexto", "descanso-curto");
-    imagem.setAttribute("src", "/imagens/descanso-curto.png");
+    alterarContexto("descanso-curto");
+    curtoBt.classList.add("active");
 });
 
 longoBt.addEventListener("click", () => {
-    html.setAttribute("data-contexto", "descanso-longo");
-    imagem.setAttribute("src", "/imagens/descanso-longo.png");
+    alterarContexto("descanso-longo");
+    longoBt.classList.add("active");
 });
+
+// setando as variaveis de forma dinamica (parametrizada)
+function alterarContexto(contexto) {
+
+    // forEach = for > usado para percorrer todos os botões com a classe declarada na inicialização da variavel
+    // o "active" é removido de todos os botões e em seguida, só um é setado novamente
+    botoes.forEach(function (contexo) {
+        contexo.classList.remove("active");
+    })
+
+    // seta o atributo "data-contexto" de acordo com o CSS passado em "contexto"
+    html.setAttribute("data-contexo", contexto);
+    // exibindo a imagem de acordo com o contexto passado (nome da imagem)
+    imagem.setAttribute("src", `/imagens/${contexto}.png`);
+
+
+    // switch para setar qual texto irá aparecer no front, de acordo com cada contexto
+    switch(contexto) {
+        case "foco":
+            titulo.innerHTML = ` 
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = ` 
+            Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = ` 
+            Hora de voltar à superficie.<br>
+                <strong class="app__title-strong">Faça uma pausa longa!</strong>`;
+            break;
+        default:
+            break;
+    }
+}
